60b39e64c475f34f9c40f530d90c9594fe10e4d1
AlexTruck <alexandr.kujba@gmail.com>
2015-03-03 23:11:42 +0200

    Add one shtick to rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,50 @@
 require 'spec_helper'
 
+include ApplicationHelper
+
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(page_title) }
+  end
+
   describe "Home page" do
     before { visit '/static_pages/home' }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title('') }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit '/static_pages/help' }
+    let(:heading)    { 'Help' }
+    let(:page_title) { '' }
 
+    it_should_behave_like "all static pages"
     it { should have_content('Help') }
-    it { should have_title('Help') }
   end
 
   describe "About page" do
     before { visit '/static_pages/about' }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { '' }
 
+    it_should_behave_like "all static pages"
     it { should have_content('About') }
-    it { should have_title('About Us') }
   end
 
   describe "Contact page" do
     before { visit '/static_pages/contact' }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { '' }
 
-    it { should have_content('Contact') }
-    it { should have_title('Contact') }
+    it_should_behave_like "all static pages"
   end
 end
