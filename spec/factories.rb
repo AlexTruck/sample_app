87cd44cce9fe90bc51383d764de514bade29d49d
AlexTruck <alexandr.kujba@gmail.com>
2015-03-06 14:17:03 +0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Alex Kuzhba"
+    email    "alexandr.kujba@gmail.com"
+    password "cvubSX31cvub"
+    password_confirmation "cvubSX31cvub"
+  end
+end
