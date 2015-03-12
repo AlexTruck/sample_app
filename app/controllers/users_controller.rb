d6057796ae6647123d58c7c7ef13df4927a4dc39
AlexTruck <alexandr.kujba@gmail.com>
2015-03-12 04:09:19 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
   end
 
   def create
@@ -18,9 +29,44 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    unless current_user.admin?
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted."
+      redirect_to users_url
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to new_session_url, notice: "Please sign in." unless signed_in?
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(static_pages_home_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(static_pages_home_url) unless current_user.admin?
   end
 end
